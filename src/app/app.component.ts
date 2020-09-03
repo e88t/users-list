d736e9e2a4fd1059b04c9c5c5ae4eac26241d82b
e88t <eugentokaryuk@gmail.com>
2020-09-03 21:21:51 +0300

    HW Lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'users-list';
+  title = 'Angular'
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
